92355ad2a5eeec496bdaee40154e56c7b97726b1
ke975 <60978634+ke975@users.noreply.github.com>
2022-10-04 17:50:51 -0600

    variables de ambiente

--- WeekDev/src/lib/apollo.js
+++ WeekDev/src/lib/apollo.js
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:'https://api-sa-east-1.hygraph.com/v2/cl4yy00cj0dyy01t686rf7d2x/master',
+    uri:import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache:new InMemoryCache()
 })
